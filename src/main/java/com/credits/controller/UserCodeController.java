8c0efa281ae3b5e81a76e5d63537bc3ad1f98579
akrasnov <none@none>
2017-12-13 10:45:19 +0100

    fixing embedded server startup

--- src/main/java/com/credits/controller/UserCodeController.java
+++ src/main/java/com/credits/controller/UserCodeController.java
@@ -1,19 +1,43 @@
 package com.credits.controller;
 
+import com.credits.compilation.SimpleInMemoryCompilator;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.*;
 
 @Controller
 @RequestMapping("/submitJava")
 public class UserCodeController {
 
-    @Resource
-    private JavaStorageService storageService;
+    private final static String CLASS_EXT = "class";
 
-    @RequestMapping(method = RequestMapping.POST)
+    @Resource
+    private SimpleInMemoryCompilator compilator;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody String doGet() {
+        return "Hello World";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
     public void doPost(@RequestParam("java") MultipartFile file) {
-        storageService.store(file);
-        //TODO: if java file is not compiled we have to compile it using SimpleInMemoryCompilator class and store with the original file
+        File source = new File(file.getName());
+        try (InputStream is = file.getInputStream(); OutputStream os = new FileOutputStream(source)) {
+            IOUtils.copy(is, os);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String ext = FilenameUtils.getExtension(source.getName());
+        if (CLASS_EXT.equalsIgnoreCase(ext)) {
+            compilator.compile(source);
+        }
     }
 }
--- src/main/java/com/credits/dao/AbstractDao.java
+++ src/main/java/com/credits/dao/AbstractDao.java
@@ -3,7 +3,6 @@ package com.credits.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Repository;
 
 public class AbstractDao {
 
