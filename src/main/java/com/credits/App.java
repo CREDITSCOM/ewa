94337111684e3b7ea178440ef861da13cf96a57c
akrasnov <none@none>
2017-12-29 11:11:19 +0100

    removing redundant exception throwing

--- src/main/java/com/credits/App.java
+++ src/main/java/com/credits/App.java
@@ -1,12 +1,11 @@
 package com.credits;
 
-import com.credits.exception.ContractExecutorException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-    public static void main(String... args) throws ContractExecutorException {
+    public static void main(String... args) {
         SpringApplication.run(App.class, args);
     }
 }
