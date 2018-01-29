81a8776620f027745a8aeddecb6dadda76015836
rustem-s <rustem.saidaliyev@gmail.com>
2018-01-29 19:11:13 +0300

    Вычисление Transaction fee percent

--- src/main/java/com/credits/wallet/desktop/controller/Form6Controller.java
+++ src/main/java/com/credits/wallet/desktop/controller/Form6Controller.java
@@ -4,6 +4,7 @@ import com.credits.wallet.desktop.App;
 import com.credits.wallet.desktop.AppState;
 import com.credits.wallet.desktop.Dictionaries;
 import com.credits.wallet.desktop.Utils;
+import com.credits.wallet.desktop.utils.Convertor;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -13,6 +14,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.util.StringConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.net.URL;
 import java.text.DecimalFormat;
 import java.text.ParseException;
@@ -22,7 +26,10 @@ import java.util.ResourceBundle;
  * Created by goncharov-eg on 18.01.2018.
  */
 public class Form6Controller extends Controller implements Initializable {
-    private static final String ERR_GETTING_BALANCE="Ошибка получения баланса";
+
+    private static Logger LOGGER = LoggerFactory.getLogger(Form6Controller.class);
+
+    private static final String ERR_GETTING_BALANCE = "Ошибка получения баланса";
 
     @FXML
     private Label labCredit;
@@ -42,6 +49,27 @@ public class Form6Controller extends Controller implements Initializable {
     @FXML
     private Label labFee;
 
+    /**
+     * c&p from Spinner
+     */
+    private <T> void commitEditorText(Spinner<T> spinner) {
+        if (!spinner.isEditable()) return;
+        String text = spinner.getEditor().getText();
+        SpinnerValueFactory<T> valueFactory = spinner.getValueFactory();
+        if (valueFactory != null) {
+            StringConverter<T> converter = valueFactory.getConverter();
+            if (converter != null) {
+                T value = converter.fromString(text);
+                valueFactory.setValue(value);
+            }
+        }
+    }
+
+    private void refreshTransactionFeePercent(Double transactionFeeValue, Double amount) {
+        AppState.transactionFeePercent = (transactionFeeValue * 100) / amount;
+        this.labFee.setText(Convertor.toString(AppState.transactionFeePercent) + " %");
+    }
+
     @FXML
     private void handleGenerate() {
         AppState.amount = numAmount.getValue();
@@ -54,7 +82,38 @@ public class Form6Controller extends Controller implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         labCredit.setText("0");
         txKey.setText("CSx5893eff21fd9c79463d127b3d3512b38dd05a42402c079e4a45d7f00a52e8");
+        labFee.setText(Convertor.toString(AppState.transactionFeePercent) + " %");
+        /*
+        // get a localized format for parsing
+        NumberFormat format = NumberFormat.getNumberInstance();
+        UnaryOperator<TextFormatter.Change> filter = c -> {
+            if (c.isContentChange()) {
+                ParsePosition parsePosition = new ParsePosition(0);
+                // NumberFormat evaluates the beginning of the text
+                format.parse(c.getControlNewText(), parsePosition);
+                if (parsePosition.getIndex() == 0 ||
+                        parsePosition.getIndex() < c.getControlNewText().length()) {
+                    // reject parsing the complete text failed
+                    return null;
+                }
+            }
+            return c;
+        };
 
+        TextFormatter<Double> amountFormatter = new TextFormatter<Double>(new DoubleStringConverter(), 0.0, filter);
+
+        numAmount.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(
+                -Double.MAX_VALUE, Double.MAX_VALUE, 0.0, 1.0));
+        numAmount.setEditable(true);
+        numAmount.getEditor().setTextFormatter(amountFormatter);
+
+        TextFormatter<Double> transactionFeeFormatter = new TextFormatter<Double>(new DoubleStringConverter(), 0.0, filter);
+
+        numFee.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(
+                -Double.MAX_VALUE, Double.MAX_VALUE, 0.0, 1.0));
+        numFee.setEditable(true);
+        numFee.getEditor().setTextFormatter(transactionFeeFormatter);
+        */
         StringConverter converter=new StringConverter<Double>() {
             private final DecimalFormat df = new DecimalFormat("#.##########");
 
@@ -90,17 +149,16 @@ public class Form6Controller extends Controller implements Initializable {
 
         SpinnerValueFactory<Double> amountValueFactory =
                 new SpinnerValueFactory.DoubleSpinnerValueFactory(-Double.MAX_VALUE, Double.MAX_VALUE,
-                        0.0, 1.0);
+                        AppState.transactionFeeValue, 0.1);
         amountValueFactory.setConverter(converter);
         numAmount.setValueFactory(amountValueFactory);
 
         SpinnerValueFactory<Double> feeValueFactory =
                 new SpinnerValueFactory.DoubleSpinnerValueFactory(-Double.MAX_VALUE, Double.MAX_VALUE,
-                        0.0006758, 1);
+                        AppState.transactionFeePercent, 0.1);
         feeValueFactory.setConverter(converter);
         numFee.setValueFactory(feeValueFactory);
 
-        labFee.setText("0.00051");
 
         // Fill coin list
         cbCoin.getItems().clear();
@@ -121,5 +179,29 @@ public class Form6Controller extends Controller implements Initializable {
                 }
             }
         });
+
+        this.numAmount.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) return;
+            //intuitive method on textField, has no effect, though
+            //spinner.getEditor().commitValue();
+            commitEditorText(this.numAmount);
+        });
+
+        this.numFee.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) return;
+            //intuitive method on textField, has no effect, though
+            //spinner.getEditor().commitValue();
+            commitEditorText(this.numFee);
+        });
+
+        this.numAmount.valueProperty().addListener((observable, oldValue, newValue) -> {
+                refreshTransactionFeePercent(this.numFee.getValue(), newValue);
+            }
+        );
+
+        this.numFee.valueProperty().addListener((observable, oldValue, newValue) -> {
+                refreshTransactionFeePercent(newValue, this.numAmount.getValue());
+            }
+        );
     }
 }
