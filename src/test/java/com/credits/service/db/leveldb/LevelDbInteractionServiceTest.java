1f91afe820f897cd5381a482308aec0319c36208
Dmitrii Pecherkin <pecherkin.dmitrii@gmail.com>
2018-02-06 17:51:12 +0300

    LevelDbInteractionServiceTest updated

--- src/test/java/com/credits/service/db/leveldb/LevelDbInteractionServiceTest.java
+++ src/test/java/com/credits/service/db/leveldb/LevelDbInteractionServiceTest.java
@@ -16,20 +16,31 @@ public class LevelDbInteractionServiceTest extends ServiceTest{
     @Test
     public void perform() throws Exception {
         System.out.println("getBalance()");
-        Double balance = service.getBalance("1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2", "CS");
+        Double balance = service.getBalance("1_GJnW172n4CdN0", "cs");
         System.out.println("getBalance=" + balance);
 
         System.out.println("getTransaction()");
-        TransactionData transaction = service.getTransaction("00000000000001adf44c7d697675870");
+        TransactionData transaction = service.getTransaction("43dd3e46-cf73-2d43-a1c5-546061a4c900");
         System.out.println("getTransaction=" + transaction);
 
         System.out.println("getTransactions()");
-        List<TransactionData> transactions = service.getTransactions("1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2", 0, 5);
+        List<TransactionData> transactions = service.getTransactions("1_GJnW172n4CdN0", 0, 20);
         System.out.println("getTransactions=" + transactions);
 
         System.out.println("getPool()");
-        PoolData poolData = service.getPool(1);
-        System.out.println("getPool=" + poolData);
+        PoolData poolData = service.getPool(0);
+        System.out.println("getPool: poolNumber= " + poolData.getPoolNumber() + " transactionsCount= "
+            + poolData.getTransactionsCount() + " hash= " + poolData.getPoolHash() + " prevHash= "
+            + poolData.getPrevPoolHash() + " time= " + poolData.getCreationTime());
+
+        System.out.println("getPoolList()");
+        List<PoolData> poolList = service.getPoolList(0, 5);
+        System.out.println("getPoolList: ");
+        for (PoolData poolData1 : poolList) {
+            System.out.println("poolNumber= " + poolData1.getPoolNumber() + " transactionsCount= "
+                + poolData1.getTransactionsCount() + " hash= " + poolData1.getPoolHash() + " prevHash= "
+                + poolData1.getPrevPoolHash() + " time= " + poolData1.getCreationTime());
+        }
     }
 
 }
