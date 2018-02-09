184071378fe7fed9d14c7e9a4c2e91ad0af672d5
egoncharov <mzeg@yandex.ru>
2018-02-09 18:26:58 +0300

    Generate transaction for new account

---
+++ src/main/java/com/credits/wallet/desktop/controller/Const.java
@@ -0,0 +1,10 @@
+package com.credits.wallet.desktop.controller;
+
+/**
+ * Created by goncharov-eg on 09.02.2018.
+ */
+public class Const {
+    public static final String CREDIT_ACCOUNT="CSx7a8b3a7d8204983d246e6edbcbe21";
+    public static final String NEW_ACCOUNT_TRANSACTION_COIN="cs";
+    public static final Double NEW_ACCOUNT_TRANSACTION_AMOUNT=100.0;
+}
--- src/main/java/com/credits/wallet/desktop/controller/Form5Controller.java
+++ src/main/java/com/credits/wallet/desktop/controller/Form5Controller.java
@@ -2,6 +2,7 @@ package com.credits.wallet.desktop.controller;
 
 import com.credits.wallet.desktop.App;
 import com.credits.wallet.desktop.AppState;
+import com.credits.wallet.desktop.Utils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,7 +13,9 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.net.URL;
 import java.util.Properties;
+import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.UUID;
 
 /**
  * Created by goncharov-eg on 18.01.2018.
@@ -54,24 +57,59 @@ public class Form5Controller extends Controller implements Initializable {
             }
         }
 
+        if (AppState.newAccount) {
+            // Transaction from credits
+
+            String hash = UUID.randomUUID().toString().replace("-", "");
+            String innerId = UUID.randomUUID().toString().replace("-", "");
+            try {
+                AppState.apiClient.transactionFlow(hash, innerId,
+                        Const.CREDIT_ACCOUNT, txPublic.getText(),
+                        Const.NEW_ACCOUNT_TRANSACTION_AMOUNT,
+                        Const.NEW_ACCOUNT_TRANSACTION_COIN);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Utils.showError("Error creating transaction " + e.toString());
+            }
+        }
+
         App.showForm("/fxml/form6.fxml", "Wallet");
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         btnBack.setVisible(!AppState.newAccount);
+        txPublic.setDisable(AppState.newAccount);
 
-        try {
-            FileInputStream fis = new FileInputStream("settings.properties");
-            Properties property = new Properties();
-            property.load(fis);
+        if (AppState.newAccount) {
+            // generate keys
+            char[] characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz".toCharArray();
+            StringBuilder sb = new StringBuilder();
+            sb.append("CSx");
+            Random random = new Random();
+            int max = characters.length - 1;
+            for (int i = 0; i < 29; i++)
+                sb.append(characters[random.nextInt(max)]);
+            txPublic.setText(sb.toString());
 
-            String publicKey = property.getProperty("public.key");
-            if (publicKey != null) {
-                txPublic.setText(publicKey);
+            sb = new StringBuilder();
+            random = new Random();
+            for (int i = 0; i < 32; i++)
+                sb.append(characters[random.nextInt(max)]);
+            txKey.setText(sb.toString());
+        } else {
+            try {
+                FileInputStream fis = new FileInputStream("settings.properties");
+                Properties property = new Properties();
+                property.load(fis);
+
+                String publicKey = property.getProperty("public.key");
+                if (publicKey != null) {
+                    txPublic.setText(publicKey);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
