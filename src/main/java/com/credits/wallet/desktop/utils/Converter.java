f7ed69b1878db8f2943bf95a73ad0efdb9fb114b
Rustem Saidaliyev <rustem.saidaliyev@gmail.com>
2018-03-21 13:32:26 +0300

    Fix Перенос строки в Signature

--- src/main/java/com/credits/wallet/desktop/utils/Converter.java
+++ src/main/java/com/credits/wallet/desktop/utils/Converter.java
@@ -1,13 +1,13 @@
 package com.credits.wallet.desktop.utils;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
-
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.Base64;
 import java.util.Locale;
+
+//import org.apache.commons.codec.binary.Base64;
 
 /**
  * Created by Rustem.Saidaliyev on 29.01.2018.
@@ -45,11 +45,11 @@ public class Converter {
     }
 
     public static String encodeToBASE64(byte[] bytes) {
-        return new BASE64Encoder().encode(bytes);
+        return Base64.getEncoder().encodeToString(bytes);
     }
 
     public static byte[] decodeFromBASE64(String string) throws IOException {
-        return new BASE64Decoder().decodeBuffer(string);
+        return Base64.getDecoder().decode(string);
     }
 
 }
