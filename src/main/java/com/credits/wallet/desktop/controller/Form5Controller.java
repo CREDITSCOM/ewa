db5f623976b4564c0277c63a1fa6c5eb43b36a54
egoncharov <mzeg@yandex.ru>
2018-04-04 15:25:06 +0300

    WALLETDESK-28 Upload file with keys

--- src/main/java/com/credits/wallet/desktop/controller/Form5Controller.java
+++ src/main/java/com/credits/wallet/desktop/controller/Form5Controller.java
@@ -7,19 +7,22 @@ import com.credits.wallet.desktop.AppState;
 import com.credits.wallet.desktop.exception.WalletDesktopException;
 import com.credits.wallet.desktop.utils.ApiUtils;
 import com.credits.wallet.desktop.utils.Utils;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 /**
@@ -35,6 +38,9 @@ public class Form5Controller extends Controller implements Initializable {
     private Button btnSaveKeys;
 
     @FXML
+    private Button btnUpload;
+
+    @FXML
     private TextField txKey;
 
     @FXML
@@ -47,34 +53,7 @@ public class Form5Controller extends Controller implements Initializable {
 
     @FXML
     private void handleOpen() {
-        AppState.account = txPublic.getText();
-        if (AppState.newAccount) {
-            // Создание системной транзакции
-            try {
-                ApiUtils.execSystemTransaction(txPublic.getText());
-            } catch (Exception e) {
-                LOGGER.error(e.getMessage(), e);
-                Utils.showError("Error creating transaction " + e.toString());
-                //return;
-            }
-        } else {
-            try {
-                byte[] publicKeyByteArr = Converter.decodeFromBASE64(txPublic.getText());
-                byte[] privateKeyByteArr = Converter.decodeFromBASE64(txKey.getText());
-                AppState.publicKey = Ed25519.bytesToPublicKey(publicKeyByteArr);
-                AppState.privateKey = Ed25519.bytesToPrivateKey(privateKeyByteArr);
-            } catch (Exception e) {
-                if (e.getMessage()!=null)
-                    Utils.showError(e.getMessage());
-                LOGGER.error(e.getMessage(), e);
-                //return;
-            }
-        }
-
-        if (validateKeys(txPublic.getText(), txKey.getText()))
-            App.showForm("/fxml/form6.fxml", "Wallet");
-        else
-            Utils.showError("Public and private keys pair is not valid");
+        open(txPublic.getText(), txKey.getText());
     }
 
     @FXML
@@ -94,8 +73,12 @@ public class Form5Controller extends Controller implements Initializable {
                         "UTF-8"
                 );
 
-                writer.println(String.format("Public: %s", Converter.encodeToBASE64(Ed25519.publicKeyToBytes(AppState.publicKey))));
-                writer.println(String.format("Private: %s", Converter.encodeToBASE64(Ed25519.privateKeyToBytes(AppState.privateKey))));
+                //writer.println(String.format("Public: %s", Converter.encodeToBASE64(Ed25519.publicKeyToBytes(AppState.publicKey))));
+                //writer.println(String.format("Private: %s", Converter.encodeToBASE64(Ed25519.privateKeyToBytes(AppState.privateKey))));
+                Map<String, String> content = new HashMap<>();
+                content.put("public", Converter.encodeToBASE64(Ed25519.publicKeyToBytes(AppState.publicKey)));
+                content.put("private", Converter.encodeToBASE64(Ed25519.privateKeyToBytes(AppState.privateKey)));
+                writer.println(new Gson().toJson(content));
 
                 writer.close();
                 Utils.showInfo(String.format("Keys successfully saved in \n\n%s", filePath));
@@ -107,10 +90,40 @@ public class Form5Controller extends Controller implements Initializable {
         }
     }
 
+    @FXML
+    private void handleUpload() {
+        FileChooser fileChooser = new FileChooser();
+        File defaultDirectory = new File(System.getProperty("user.dir"));
+        fileChooser.setInitialDirectory(defaultDirectory);
+        fileChooser.setInitialFileName("wallet-keys.txt");
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+            try {
+                InputStream is = new FileInputStream(file);
+                BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+                String line = buf.readLine();
+                StringBuilder sb = new StringBuilder();
+                while (line != null) {
+                    sb.append(line);
+                    line = buf.readLine();
+                }
+                JsonObject jObject = new JsonParser().parse(sb.toString()).getAsJsonObject();
+                String pubKey=jObject.get("public").getAsString();
+                String privKey=jObject.get("private").getAsString();
+
+                open(pubKey, privKey);
+            } catch (Exception e) {
+                LOGGER.error(e.getMessage(), e);
+                Utils.showError("Error reading keys from file " + file.getAbsolutePath() + " " + e.toString());
+            }
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         btnBack.setVisible(!AppState.newAccount);
         btnSaveKeys.setVisible(AppState.newAccount);
+        btnUpload.setVisible(!AppState.newAccount);
         txPublic.setDisable(AppState.newAccount);
         txKey.setDisable(AppState.newAccount);
 
@@ -120,16 +133,47 @@ public class Form5Controller extends Controller implements Initializable {
         }
     }
 
+    private void open (String pubKey, String privKey) {
+        AppState.account = pubKey;
+        if (AppState.newAccount) {
+            // Создание системной транзакции
+            try {
+                ApiUtils.execSystemTransaction(pubKey);
+            } catch (Exception e) {
+                LOGGER.error(e.getMessage(), e);
+                Utils.showError("Error creating transaction " + e.toString());
+                //return;
+            }
+        } else {
+            try {
+                byte[] publicKeyByteArr = Converter.decodeFromBASE64(pubKey);
+                byte[] privateKeyByteArr = Converter.decodeFromBASE64(privKey);
+                AppState.publicKey = Ed25519.bytesToPublicKey(publicKeyByteArr);
+                AppState.privateKey = Ed25519.bytesToPrivateKey(privateKeyByteArr);
+            } catch (Exception e) {
+                if (e.getMessage() != null)
+                    Utils.showError(e.getMessage());
+                LOGGER.error(e.getMessage(), e);
+                //return;
+            }
+        }
+
+        if (validateKeys(pubKey, privKey))
+            App.showForm("/fxml/form6.fxml", "Wallet");
+        else
+            Utils.showError("Public and private keys pair is not valid");
+    }
+
     private boolean validateKeys(String publicKey, String privateKey) {
         try {
             byte[] publicKeyByteArr = Converter.decodeFromBASE64(publicKey);
             byte[] privateKeyByteArr = Converter.decodeFromBASE64(privateKey);
 
-            if (privateKeyByteArr.length<=32)
+            if (privateKeyByteArr.length <= 32)
                 return false;
 
-            for (int i=0; i<publicKeyByteArr.length && i<privateKeyByteArr.length-32; i++) {
-                if (publicKeyByteArr[i]!=privateKeyByteArr[i+32])
+            for (int i = 0; i < publicKeyByteArr.length && i < privateKeyByteArr.length - 32; i++) {
+                if (publicKeyByteArr[i] != privateKeyByteArr[i + 32])
                     return false;
             }
 
