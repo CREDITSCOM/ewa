d984e4f9207acbb21192112cd7f72939844bfc99
egoncharov <mzeg@yandex.ru>
2018-06-04 16:59:35 +0300

    Smart contract search

--- src/main/java/com/credits/wallet/desktop/controller/SmartContractController.java
+++ src/main/java/com/credits/wallet/desktop/controller/SmartContractController.java
@@ -1,57 +1,19 @@
 package com.credits.wallet.desktop.controller;
 
-import com.credits.common.utils.Converter;
-import com.credits.crypto.Ed25519;
-import com.credits.leveldb.client.ApiClient;
-import com.credits.leveldb.client.data.ApiResponseData;
 import com.credits.leveldb.client.data.SmartContractData;
 import com.credits.wallet.desktop.App;
 import com.credits.wallet.desktop.AppState;
-import com.credits.wallet.desktop.struct.ErrorCodeTabRow;
-import com.credits.wallet.desktop.thrift.executor.APIResponse;
-import com.credits.wallet.desktop.thrift.executor.ContractExecutor;
-import com.credits.wallet.desktop.thrift.executor.ContractFile;
-import com.credits.wallet.desktop.utils.ApiUtils;
-import com.credits.wallet.desktop.utils.EclipseJdt;
-import com.credits.wallet.desktop.utils.SimpleInMemoryCompilator;
 import com.credits.wallet.desktop.utils.Utils;
-import javafx.concurrent.Task;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.*;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.Pane;
-import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.transport.TSocket;
-import org.apache.thrift.transport.TTransport;
-import org.eclipse.jdt.core.compiler.IProblem;
-import org.eclipse.jdt.core.dom.*;
-import org.fxmisc.richtext.CodeArea;
-import org.fxmisc.richtext.LineNumberFactory;
-import org.fxmisc.richtext.model.StyleSpans;
-import org.fxmisc.richtext.model.StyleSpansBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
 import java.net.URL;
-import java.time.Duration;
 import java.util.*;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Created by goncharov-eg on 30.01.2018.
@@ -65,6 +27,9 @@ public class SmartContractController extends Controller implements Initializable
 
     @FXML
     private TextField txAddress;
+
+    @FXML
+    private TextArea taCode;
 
     @FXML
     private TreeView<Label> contractsTree;
@@ -85,9 +50,8 @@ public class SmartContractController extends Controller implements Initializable
         try {
             SmartContractData smartContractData = AppState.apiClient.getSmartContract(address);
 
-
-
-            // this.address.setText(smartContractData.getAddress);
+            this.address.setText(address);
+            this.taCode.setText(smartContractData.getSourceCode());
 
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
@@ -100,6 +64,9 @@ public class SmartContractController extends Controller implements Initializable
     @SuppressWarnings("unchecked")
     public void initialize(URL location, ResourceBundle resources) {
         this.contractsTree.setRoot(null);
+
+        //2DO
+        //AppState.apiClient.
     }
 
     @FXML
