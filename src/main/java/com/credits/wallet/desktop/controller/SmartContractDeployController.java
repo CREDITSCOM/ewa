117c88ad96d5e18b0baa1d9e4bb4270e2c6e8aea
Igor Goryunov <kongostream@gmail.com>
2018-10-11 21:13:34 +0300

    FEATURE: backspace remove 4 symbols after tab

--- src/main/java/com/credits/wallet/desktop/controller/SmartContractDeployController.java
+++ src/main/java/com/credits/wallet/desktop/controller/SmartContractDeployController.java
@@ -23,7 +23,6 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import org.eclipse.jdt.core.compiler.IProblem;
 import org.eclipse.jdt.core.dom.BodyDeclaration;
@@ -49,24 +48,17 @@ import static org.fxmisc.wellbehaved.event.EventPattern.keyPressed;
 //TODO: This class is a GODZILLA please refactor it ASAP!
 public class SmartContractDeployController extends Controller implements Initializable {
 
-    private static Logger LOGGER = LoggerFactory.getLogger(SmartContractDeployController.class);
-
     private static final String DEFAULT_SOURCE_CODE =
-        "public class Contract extends SmartContract {\n" + "\n" + "    public Contract() {\n" +
-            "        total = 0;\n" + "    }" + "\n" + "}";
-//    private static final String NON_CHANGED_STR = "public class Contract extends SmartContract {";
-
+        "public class Contract extends SmartContract {\n" + "\n" + "    public Contract() {\n" + "        total = 0;\n" + "    }" + "\n" + "}";
     private static final String[] PARENT_METHODS =
-        new String[] {"double total", "Double getBalance(String address, String currency)",
-            "TransactionData getTransaction(String transactionId)",
-            "List<TransactionData> getTransactions(String address, long offset, long limit)",
-            "List<PoolData> getPoolList(long offset, long limit)", "PoolData getPool(String poolNumber)",
-            "void sendTransaction(String account, String target, Double amount, String currency)"};
-
+        new String[] {"double total", "Double getBalance(String address, String currency)", "TransactionData getTransaction(String transactionId)",
+            "List<TransactionData> getTransactions(String address, long offset, long limit)", "List<PoolData> getPoolList(long offset, long limit)",
+            "PoolData getPool(String poolNumber)", "void sendTransaction(String account, String target, Double amount, String currency)"};
+    //    private static final String NON_CHANGED_STR = "public class Contract extends SmartContract {";
+    private static Logger LOGGER = LoggerFactory.getLogger(SmartContractDeployController.class);
     private CodeArea codeArea;
     private TableView<ErrorCodeTabRow> tabErrors;
-
-    private String prevCode;
+    private int tabCount;
 
     @FXML
     private Pane paneCode;
@@ -84,71 +76,114 @@ public class SmartContractDeployController extends Controller implements Initial
         }
         AppState.executor = Executors.newSingleThreadExecutor();
 
-        this.prevCode = DEFAULT_SOURCE_CODE;
+        codeArea = SmartContractUtils.initCodeArea(paneCode);
 
-        this.codeArea = SmartContractUtils.initCodeArea(this.paneCode);
+        codeArea.setOnKeyPressed(k -> {
+            KeyCode code = k.getCode();
+            if (code != KeyCode.TAB){
+                if(code.isLetterKey() || code.isDigitKey() || code.isNavigationKey() || code.isWhitespaceKey()){
+                    tabCount = 0;
+                }
+            }
 
-        this.codeArea.setOnKeyPressed(ke -> {
-            if (ke.isControlDown() && ke.getCode().equals(KeyCode.SPACE)) {
+            if (k.isControlDown() && k.getCode().equals(KeyCode.SPACE)) {
                 codeCompletionPopup();
             }
         });
 
-        InputMap<KeyEvent> im = InputMap.consume(
-            keyPressed(KeyCode.TAB),
-            e -> codeArea.replaceSelection("    "));
-        Nodes.addInputMap(codeArea, im);
+        Nodes.addInputMap(codeArea, InputMap.consume(keyPressed(KeyCode.TAB), e -> {
+            tabCount++;
+            codeArea.replaceSelection("    ");
+        }));
 
-//        this.codeArea.richChanges().filter(ch -> !ch.getInserted().equals(ch.getRemoved())) // XXX
-//            .subscribe(change -> {
-//                String curCode = this.codeArea.getText();
-//
-////                // Replace TAB to 4 spaces
-////                if (curCode.contains("\t")) {
-////                    this.codeArea.replaceText(0, curCode.length(), curCode.replace("\t", "    "));
-////                    curCode = this.codeArea.getText();
-////                }
-//
-////                if (!curCode.contains(NON_CHANGED_STR)) {
-////                    this.codeArea.replaceText(0, curCode.length(), this.prevCode);
-////                } else {
-////                    int i1 = curCode.indexOf(NON_CHANGED_STR);
-////                    if (curCode.indexOf(NON_CHANGED_STR, i1 + 1) > 0) {
-////                        this.codeArea.replaceText(0, curCode.length(), this.prevCode);
-////                    }
-////                }
-//                this.prevCode = this.codeArea.getText();
-//            });
+        Nodes.addInputMap(codeArea, InputMap.consume(keyPressed(KeyCode.BACK_SPACE), e -> {
+            if (tabCount > 0) {
+                for (int i = 0; i < 4; i++) {
+                    codeArea.deletePreviousChar();
+                }
+                tabCount--;
+            } else {
+                codeArea.deletePreviousChar();
+            }
+        }));
 
+        codeArea.replaceText(0, 0, DEFAULT_SOURCE_CODE);
 
-        this.codeArea.replaceText(0, 0, DEFAULT_SOURCE_CODE);
-
-        this.tabErrors = new TableView<>();
-        this.tabErrors.setPrefHeight(this.scCodePanel.getPrefHeight() * 0.3);
-        this.tabErrors.setPrefWidth(this.scCodePanel.getPrefWidth());
+        tabErrors = new TableView<>();
+        tabErrors.setPrefHeight(scCodePanel.getPrefHeight() * 0.3);
+        tabErrors.setPrefWidth(scCodePanel.getPrefWidth());
 
         TableColumn<ErrorCodeTabRow, String> tabErrorsColLine = new TableColumn<>();
         tabErrorsColLine.setText("Line");
         tabErrorsColLine.setCellValueFactory(new PropertyValueFactory<>("line"));
-        tabErrorsColLine.setPrefWidth(this.scCodePanel.getPrefWidth() * 0.1);
+        tabErrorsColLine.setPrefWidth(scCodePanel.getPrefWidth() * 0.1);
 
         TableColumn<ErrorCodeTabRow, String> tabErrorsColText = new TableColumn<>();
         tabErrorsColText.setText("Error");
         tabErrorsColText.setCellValueFactory(new PropertyValueFactory<>("text"));
-        tabErrorsColText.setPrefWidth(this.scCodePanel.getPrefWidth() * 0.9);
+        tabErrorsColText.setPrefWidth(scCodePanel.getPrefWidth() * 0.9);
 
-        this.tabErrors.getColumns().add(tabErrorsColLine);
-        this.tabErrors.getColumns().add(tabErrorsColText);
+        tabErrors.getColumns().add(tabErrorsColLine);
+        tabErrors.getColumns().add(tabErrorsColText);
 
-        this.tabErrors.setOnMousePressed(event -> {
+        tabErrors.setOnMousePressed(event -> {
             if (event.isPrimaryButtonDown()) {
-                ErrorCodeTabRow tabRow = this.tabErrors.getSelectionModel().getSelectedItem();
+                ErrorCodeTabRow tabRow = tabErrors.getSelectionModel().getSelectedItem();
                 if (tabRow != null) {
                     positionCursorToLine(Integer.valueOf(tabRow.getLine()));
                 }
             }
         });
 
+    }
+
+    private void codeCompletionPopup() {
+        ContextMenu contextMenu = new ContextMenu();
+
+        String word = "";
+        int pos = codeArea.getCaretPosition() - 1;
+        String txt = codeArea.getText();
+        while (pos > 0 && !txt.substring(pos, pos + 1).equals(" ") && !txt.substring(pos, pos + 1).equals("\r") && !txt.substring(pos, pos + 1).equals("\n")) {
+            word = txt.substring(pos, pos + 1) + word;
+            pos--;
+        }
+
+        for (String method : PARENT_METHODS) {
+            if (word.trim().isEmpty() || method.toUpperCase().indexOf(word.trim().toUpperCase()) > 0) {
+                MenuItem action = new MenuItem(method);
+                contextMenu.getItems().add(action);
+                action.setOnAction(event -> {
+                    int pos1 = codeArea.getCaretPosition();
+                    String txtToIns = SourceCodeUtils.normalizeMethodName(method);
+                    codeArea.replaceText(pos1, pos1, txtToIns);
+                });
+            }
+        }
+
+        if (contextMenu.getItems().isEmpty()) {
+            MenuItem action = new MenuItem("No suggestions");
+            contextMenu.getItems().add(action);
+        }
+
+        contextMenu.show(codeArea, codeArea.getCaretBounds().get().getMaxX(), codeArea.getCaretBounds().get().getMaxY());
+    }
+
+    private void positionCursorToLine(int line) {
+        char[] text = codeArea.getText().toCharArray();
+        int pos = 0;
+        int curLine = 1;
+        while (pos < text.length) {
+            if (line <= curLine) {
+                break;
+            }
+            if (text[pos] == '\n') {
+                curLine++;
+            }
+            pos++;
+        }
+        codeArea.displaceCaret(pos);
+        codeArea.showParagraphAtTop(Math.max(0, line - 5));
+        codeArea.requestFocus();
     }
 
     @FXML
@@ -163,9 +198,9 @@ public class SmartContractDeployController extends Controller implements Initial
     @FXML
     private void handleDeploy() {
         String token = SourceCodeUtils.generateSmartContractToken();
-        String className = SourceCodeUtils.parseClassName(this.codeArea.getText(), "SmartContract");
+        String className = SourceCodeUtils.parseClassName(codeArea.getText(), "SmartContract");
         try {
-            String javaCode = SourceCodeUtils.normalizeSourceCode(this.codeArea.getText());
+            String javaCode = SourceCodeUtils.normalizeSourceCode(codeArea.getText());
             byte[] byteCode = SimpleInMemoryCompiler.compile(javaCode, className, token);
             String hashState = ApiUtils.generateSmartContractHashState(byteCode);
 
@@ -183,8 +218,8 @@ public class SmartContractDeployController extends Controller implements Initial
 
     private void refreshClassMembersTree() {
 
-        this.classTreeView.setRoot(null);
-        String sourceCode = this.codeArea.getText();
+        classTreeView.setRoot(null);
+        String sourceCode = codeArea.getText();
         String className = SourceCodeUtils.parseClassName(sourceCode);
         Label labelRoot = new Label(className);
         TreeItem<Label> treeRoot = new TreeItem<>(labelRoot);
@@ -215,87 +250,36 @@ public class SmartContractDeployController extends Controller implements Initial
         });
 
         treeRoot.setExpanded(true);
-        this.classTreeView.setRoot(treeRoot);
+        classTreeView.setRoot(treeRoot);
     }
 
     @FXML
     @SuppressWarnings("unchecked")
     private void checkButtonAction() {
-        String sourceCode = this.codeArea.getText();
+        String sourceCode = codeArea.getText();
 
         IProblem[] problemArr = EclipseJdt.checkSyntax(sourceCode);
 
         if (problemArr.length > 0) {
-            this.tabErrors.getItems().clear();
+            tabErrors.getItems().clear();
 
             for (IProblem p : problemArr) {
                 ErrorCodeTabRow tr = new ErrorCodeTabRow();
                 tr.setLine(Integer.toString(p.getSourceLineNumber()));
                 tr.setText(p.getMessage());
-                this.tabErrors.getItems().add(tr);
+                tabErrors.getItems().add(tr);
             }
 
-            this.codeArea.setPrefHeight(this.paneCode.getPrefHeight() * 0.7);
-            this.paneCode.getChildren().clear();
-            this.paneCode.getChildren().add(this.codeArea);
-            this.paneCode.getChildren().add(this.tabErrors);
-            this.paneCode.getChildren().get(1).setLayoutY(this.paneCode.getPrefHeight() * 0.7);
+            codeArea.setPrefHeight(paneCode.getPrefHeight() * 0.7);
+            paneCode.getChildren().clear();
+            paneCode.getChildren().add(codeArea);
+            paneCode.getChildren().add(tabErrors);
+            paneCode.getChildren().get(1).setLayoutY(paneCode.getPrefHeight() * 0.7);
         } else {
-            this.codeArea.setPrefHeight(this.paneCode.getPrefHeight());
-            this.paneCode.getChildren().clear();
-            this.paneCode.getChildren().add(this.codeArea);
+            codeArea.setPrefHeight(paneCode.getPrefHeight());
+            paneCode.getChildren().clear();
+            paneCode.getChildren().add(codeArea);
             FormUtils.showInfo("Everything is OK");
         }
-    }
-
-    private void positionCursorToLine(int line) {
-        char[] text = this.codeArea.getText().toCharArray();
-        int pos = 0;
-        int curLine = 1;
-        while (pos < text.length) {
-            if (line <= curLine) {
-                break;
-            }
-            if (text[pos] == '\n') {
-                curLine++;
-            }
-            pos++;
-        }
-        this.codeArea.displaceCaret(pos);
-        this.codeArea.showParagraphAtTop(Math.max(0, line - 5));
-        this.codeArea.requestFocus();
-    }
-
-    private void codeCompletionPopup() {
-        ContextMenu contextMenu = new ContextMenu();
-
-        String word = "";
-        int pos = this.codeArea.getCaretPosition() - 1;
-        String txt = this.codeArea.getText();
-        while (pos > 0 && !txt.substring(pos, pos + 1).equals(" ") && !txt.substring(pos, pos + 1).equals("\r") &&
-            !txt.substring(pos, pos + 1).equals("\n")) {
-            word = txt.substring(pos, pos + 1) + word;
-            pos--;
-        }
-
-        for (String method : PARENT_METHODS) {
-            if (word.trim().isEmpty() || method.toUpperCase().indexOf(word.trim().toUpperCase()) > 0) {
-                MenuItem action = new MenuItem(method);
-                contextMenu.getItems().add(action);
-                action.setOnAction(event -> {
-                    int pos1 = this.codeArea.getCaretPosition();
-                    String txtToIns = SourceCodeUtils.normalizeMethodName(method);
-                    this.codeArea.replaceText(pos1, pos1, txtToIns);
-                });
-            }
-        }
-
-        if (contextMenu.getItems().isEmpty()) {
-            MenuItem action = new MenuItem("No suggestions");
-            contextMenu.getItems().add(action);
-        }
-
-        contextMenu.show(this.codeArea, this.codeArea.getCaretBounds().get().getMaxX(),
-            this.codeArea.getCaretBounds().get().getMaxY());
     }
 }
