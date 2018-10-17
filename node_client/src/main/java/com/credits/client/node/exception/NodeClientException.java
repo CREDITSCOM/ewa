aa3384a87af3425a1e5910b42a15c5c8f11a31b8
Igor Goryunov <kongostream@gmail.com>
2018-10-17 12:56:36 +0300

    IMPROVEMENT: move packages between modules
    -move thrift.executor from node_client to executor_client
    -move thrift.variant from node_client to general

---
+++ node_client/src/main/java/com/credits/client/node/exception/NodeClientException.java
@@ -0,0 +1,18 @@
+package com.credits.client.node.exception;
+
+
+import com.credits.general.exception.CreditsException;
+
+/**
+ * Created by Rustem.Saidaliyev on 28.06.2018.
+ */
+public class NodeClientException extends CreditsException {
+
+    public NodeClientException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public NodeClientException(Exception e) {
+        super(e);
+    }
+}
