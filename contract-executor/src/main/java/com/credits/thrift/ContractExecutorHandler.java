148b337e00b2de8f9aefe457d42e22e113c164f3
Igor Goryunov <kongostream@gmail.com>
2018-11-02 13:52:55 +0300

    BUG-FIX: response contractHash log message

--- contract-executor/src/main/java/com/credits/thrift/ContractExecutorHandler.java
+++ contract-executor/src/main/java/com/credits/thrift/ContractExecutorHandler.java
@@ -54,7 +54,7 @@ public class ContractExecutorHandler implements ContractExecutor.Iface {
             response.setCode((byte) 1);
             response.setMessage(e.getMessage());
         }
-        logger.debug("execute --> contractStateHash {} {}", contractState.hashCode(), response);
+        logger.debug("execute --> contractStateHash {} {}", response.hashCode(), response);
         return response;
     }
 
