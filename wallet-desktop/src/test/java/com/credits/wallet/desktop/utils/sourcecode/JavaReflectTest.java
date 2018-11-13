ac9271892afa71554d9c64da412e0ef5f7f44aaf
Rustem Saidaliyev <rustem.saidaliyev@gmail.com>
2018-11-13 14:38:30 +0300

    NODE-224 Добавить авто-дополнение кода в компоненте code-area модуля wallet-desktop

---
+++ wallet-desktop/src/test/java/com/credits/wallet/desktop/utils/sourcecode/JavaReflectTest.java
@@ -0,0 +1,28 @@
+package com.credits.wallet.desktop.utils.sourcecode;
+
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+
+
+public class JavaReflectTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JavaReflectTest.class);
+
+    @Test
+    public void getSignatureTest01() {
+        try {
+            Class<?> clazz = Class.forName("SmartContract");
+            Map<Method, String> methods = JavaReflect.getDeclaredMethods(clazz);
+            methods.forEach((k, v) -> {
+                LOGGER.info(v);
+            });
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
