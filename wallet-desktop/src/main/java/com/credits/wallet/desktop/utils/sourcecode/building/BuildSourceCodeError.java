52c2b27044acf44e3bd4f36cbbbceb1cfd7ed136
vm@credits.com <vm@credits.com>
2018-12-06 11:34:26 +0300

    [IMPROVEMENT] - refactoring classes for build

---
+++ wallet-desktop/src/main/java/com/credits/wallet/desktop/utils/sourcecode/building/BuildSourceCodeError.java
@@ -0,0 +1,25 @@
+package com.credits.wallet.desktop.utils.sourcecode.building;
+
+/**
+ * Created by goncharov-eg on 02.03.2018.
+ */
+public class BuildSourceCodeError {
+    private String line;
+    private String text;
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ wallet-desktop/src/main/java/com/credits/wallet/desktop/utils/sourcecode/building/CompilationResult.java
@@ -0,0 +1,31 @@
+package com.credits.wallet.desktop.utils.sourcecode.building;
+
+import com.credits.wallet.desktop.utils.compiler.model.CompilationPackage;
+
+import java.util.List;
+
+public class CompilationResult {
+    private List<BuildSourceCodeError> errors;
+    private CompilationPackage compilationPackage;
+
+    public CompilationResult(CompilationPackage compilationPackage, List<BuildSourceCodeError> listOfError) {
+        this.errors = listOfError;
+        this.compilationPackage = compilationPackage;
+    }
+
+    public List<BuildSourceCodeError> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<BuildSourceCodeError> errors) {
+        this.errors = errors;
+    }
+
+    public CompilationPackage getCompilationPackage() {
+        return compilationPackage;
+    }
+
+    public void setCompilationPackage(CompilationPackage compilationPackage) {
+        this.compilationPackage = compilationPackage;
+    }
+}
