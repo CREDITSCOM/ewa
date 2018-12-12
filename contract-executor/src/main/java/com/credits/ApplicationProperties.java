ec55826ab650e838c94ce352f740cf1066cd299e
vm@credits.com <vm@credits.com>
2018-12-12 12:22:08 +0300

    [IMPROVEMENT] - load property from file

--- contract-executor/src/main/java/com/credits/ApplicationProperties.java
+++ contract-executor/src/main/java/com/credits/ApplicationProperties.java
@@ -1,7 +1,28 @@
 package com.credits;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ApplicationProperties {
     public String apiHost = "localhost";
     public int apiPort = 9090;
-    public int executorPort = 9080;
+    public int executorPort = 9070;
+
+    public ApplicationProperties(){
+        Properties properties = new Properties();
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("settings.properties");
+            properties.load(fis);
+            String publicKey = properties.getProperty("contract.executor.public.key");
+            System.setProperty(publicKey, properties.getProperty("contract.executor.private.key"));
+            apiHost = properties.getProperty("contract.executor.host");
+            apiPort = Integer.parseInt(properties.getProperty("node.api.port"));
+            executorPort = Integer.parseInt(properties.getProperty("contract.executor.port"));
+        } catch (IOException e) {
+            throw new RuntimeException("can't load propertyFile", e);
+        }
+
+    }
 }
