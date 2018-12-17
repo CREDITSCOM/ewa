40ffb924f354883bbe79d866186c76bd566aa1a8
vm@credits.com <vm@credits.com>
2018-12-17 20:14:38 +0300

    [BUG-FIX] - fix for send transaction

--- wallet-desktop/src/main/java/com/credits/wallet/desktop/utils/SmartContractsUtils.java
+++ wallet-desktop/src/main/java/com/credits/wallet/desktop/utils/SmartContractsUtils.java
@@ -1,23 +1,42 @@
 package com.credits.wallet.desktop.utils;
 
-import com.credits.client.node.crypto.Ed25519;
 import com.credits.general.crypto.Md5;
 import com.credits.general.exception.CreditsException;
 import org.apache.commons.lang3.ArrayUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.security.KeyPair;
-import java.security.PublicKey;
+import java.util.Arrays;
 
 import static com.credits.general.crypto.Blake2S.generateHash;
 import static com.credits.general.util.GeneralConverter.byteArrayToHex;
 import static com.credits.general.util.GeneralConverter.toByteArray;
-import static org.apache.commons.lang3.ArrayUtils.*;
+import static com.credits.general.util.GeneralConverter.toByteArrayLittleEndian;
+import static org.apache.commons.lang3.ArrayUtils.addAll;
 
 public class SmartContractsUtils {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(SmartContractsUtils.class);
+    private static byte[] bytes;
+
+
     public static byte[] generateSmartContractAddress(byte[] deployerAddress, long transactionId, byte[] bytecode) {
-        byte[] seed = addAll(deployerAddress, toByteArray(transactionId));
+
+        bytes = toByteArray(transactionId);
+
+        byte[] sliceId = Arrays.copyOfRange(toByteArray(transactionId), 2, 8);
+        ArrayUtils.reverse(sliceId);
+
+        byte[] seed = addAll(deployerAddress, toByteArrayLittleEndian(sliceId,sliceId.length));
         seed = addAll(seed, bytecode);
-        return generateHash(seed);
+        seed = toByteArrayLittleEndian(seed, seed.length);
+        LOGGER.info("Generate smart contract address:\n");
+
+        for(Byte b: seed){
+            System.out.print(((0xff) & b) + ", ");
+        }
+        byte[] bytes = generateHash(seed);
+        return bytes;
     }
 
     public static String generateSmartContractHashState(byte[] byteCode) throws CreditsException {
