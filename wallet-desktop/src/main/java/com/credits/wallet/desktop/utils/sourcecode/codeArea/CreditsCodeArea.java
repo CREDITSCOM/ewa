1133dce507cd54199ed68fdd0a791f9e093fbed2
Igor Goryunov <kongostream@gmail.com>
2019-02-18 15:51:39 +0300

    BUG-FIX: remove initiator from templates

--- wallet-desktop/src/main/java/com/credits/wallet/desktop/utils/sourcecode/codeArea/CreditsCodeArea.java
+++ wallet-desktop/src/main/java/com/credits/wallet/desktop/utils/sourcecode/codeArea/CreditsCodeArea.java
@@ -44,7 +44,7 @@ public class CreditsCodeArea extends CodeArea {
     private static final String NEW_LINE_SYMBOL = "\n";
     private static final String TAB_STRING = StringUtils.repeat(" ", TAB_SIZE);
     public static final String DEFAULT_SOURCE_CODE =
-            "public class Contract extends SmartContract {\n" + "\n" + TAB_STRING + "public Contract(String initiator) {\n" + TAB_STRING + TAB_STRING + "super(initiator);\n" + TAB_STRING + "}" + "\n" + "}";
+            "public class Contract extends SmartContract {\n" + "\n" + TAB_STRING + "public Contract() {\n" + TAB_STRING + TAB_STRING + "\n}" + "\n" + "}";
 
     private static int tabCount;
     private ExecutorService codeAreaHighlightExecutor = Executors.newSingleThreadExecutor();
