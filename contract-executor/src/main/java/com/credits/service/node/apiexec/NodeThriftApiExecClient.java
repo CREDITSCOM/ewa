ab550895602a4aadd1647940815e74947d9020e3
Igor Goryunov <kongostream@gmail.com>
2019-05-15 14:34:04 +0300

    IMPROVEMENT: remove unnecessary method

--- contract-executor/src/main/java/com/credits/service/node/apiexec/NodeThriftApiExecClient.java
+++ contract-executor/src/main/java/com/credits/service/node/apiexec/NodeThriftApiExecClient.java
@@ -12,20 +12,13 @@ import com.credits.general.thrift.ThriftClientPool;
 import org.apache.thrift.TException;
 
 import java.nio.ByteBuffer;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-/**
- * Created by Rustem.Saidaliyev on 01.02.2018.
- */
 public class NodeThriftApiExecClient implements NodeThriftApiExec {
     private static volatile NodeThriftApiExecClient instance;
-    private final ExecutorService threadPoolExecutor;
     private final ThriftClientPool<APIEXEC.Client> pool;
 
     private NodeThriftApiExecClient(String apiServerHost, Integer apiServerPort) {
         pool = new ThriftClientPool<>(APIEXEC.Client::new, apiServerHost, apiServerPort);
-        threadPoolExecutor = Executors.newCachedThreadPool();
     }
 
     public static NodeThriftApiExecClient getInstance(String host, Integer port) {
@@ -39,10 +32,6 @@ public class NodeThriftApiExecClient implements NodeThriftApiExec {
             }
         }
         return localInstance;
-    }
-
-    public ThriftClientPool<APIEXEC.Client> getPool() {
-        return pool;
     }
 
     @Override
