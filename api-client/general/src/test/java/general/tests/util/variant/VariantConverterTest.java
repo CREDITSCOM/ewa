e8cc33642fcbfe73e1d63d80832e14496c0b82ed
Igor Goryunov <kongostream@gmail.com>
2019-05-16 17:07:26 +0300

    IMPROVEMENT: add variant BigDecimal test case

--- api-client/general/src/test/java/general/tests/util/variant/VariantConverterTest.java
+++ api-client/general/src/test/java/general/tests/util/variant/VariantConverterTest.java
@@ -8,31 +8,19 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.math.BigDecimal;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import static com.credits.general.serialize.Serializer.serialize;
-import static com.credits.general.thrift.generated.Variant._Fields.V_ARRAY;
-import static com.credits.general.thrift.generated.Variant._Fields.V_BOOLEAN_BOX;
-import static com.credits.general.thrift.generated.Variant._Fields.V_DOUBLE_BOX;
-import static com.credits.general.thrift.generated.Variant._Fields.V_FLOAT_BOX;
-import static com.credits.general.thrift.generated.Variant._Fields.V_INT;
-import static com.credits.general.thrift.generated.Variant._Fields.V_INT_BOX;
-import static com.credits.general.thrift.generated.Variant._Fields.V_LIST;
-import static com.credits.general.thrift.generated.Variant._Fields.V_LONG_BOX;
-import static com.credits.general.thrift.generated.Variant._Fields.V_MAP;
-import static com.credits.general.thrift.generated.Variant._Fields.V_NULL;
-import static com.credits.general.thrift.generated.Variant._Fields.V_OBJECT;
-import static com.credits.general.thrift.generated.Variant._Fields.V_SET;
-import static com.credits.general.thrift.generated.Variant._Fields.V_SHORT_BOX;
-import static com.credits.general.thrift.generated.Variant._Fields.V_STRING;
-import static com.credits.general.thrift.generated.Variant._Fields.V_VOID;
+import static com.credits.general.thrift.generated.Variant._Fields.*;
 import static com.credits.general.util.Utils.getClassType;
 import static com.credits.general.util.variant.VariantConverter.VOID_TYPE_VALUE;
 import static general.tests.TestHelper.ExampleClass;
+import static java.math.RoundingMode.DOWN;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -40,73 +28,58 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VariantConverterTest {
 
     private static ExampleClass exampleClass = new ExampleClass(23);
-    private static ArrayList listWithStrings = new ArrayList() {{
-        add("1");
-        add("2");
-    }};
-    private static ArrayList listWithObjects = new ArrayList() {{
-        add(new Variant(V_NULL, ""));
-        add(new Variant(V_OBJECT, new object(exampleClass.getClass().getName(), ByteBuffer.wrap(serialize(exampleClass)))));
-    }};
-    private static HashMap mapStringInteger =
-        new HashMap() {{
-            put("one", 1);
-        }};
-    private static HashSet stringSet = new HashSet() {{
-        add("object");
-    }};
-    private static Object[] objectsArray = new ExampleClass[] {null, exampleClass};
-    private static int[] intsArray = new int[] {1, 2, 3};
-    private static Integer[] integersArray = new Integer[] {1, 2, 3};
+    private static BigDecimal exampleBigDecimal = new BigDecimal(5.55).setScale(2, DOWN);
+    private static List<String> listWithStrings = List.of("1", "2");
 
-    @SuppressWarnings("unchecked")
+    private static Map mapStringInteger = Map.of("one", 1);
+    private static Set<String> stringSet = Set.of("object");
+    private static Object[] objectsArray = new ExampleClass[]{null, exampleClass};
+    private static int[] intsArray = new int[]{1, 2, 3};
+    private static Integer[] integersArray = new Integer[]{1, 2, 3};
+
     static Stream<Object> provideObjectsForMapping() {
         return Stream.of(
-            Arguments.of(getClassType(""), null, V_NULL, getClassType("")),
-            Arguments.of(Void.TYPE.getName(), Void.TYPE, V_VOID, VOID_TYPE_VALUE),
-            Arguments.of(getClassType(Boolean.TRUE), Boolean.TRUE, V_BOOLEAN_BOX, Boolean.TRUE),
-            Arguments.of(getClassType(Integer.MAX_VALUE), Integer.MAX_VALUE, V_INT_BOX, Integer.MAX_VALUE),
-            Arguments.of(getClassType(Short.MAX_VALUE), Short.MAX_VALUE, V_SHORT_BOX, Short.MAX_VALUE),
-            Arguments.of(getClassType(Long.MAX_VALUE), Long.MAX_VALUE, V_LONG_BOX, Long.MAX_VALUE),
-            Arguments.of(getClassType(Float.MAX_VALUE), Float.MAX_VALUE, V_FLOAT_BOX, (double) Float.MAX_VALUE),
-            Arguments.of(getClassType(Double.MAX_VALUE), Double.MAX_VALUE, V_DOUBLE_BOX, Double.MAX_VALUE),
-            Arguments.of(getClassType("String"), "String", V_STRING, "String"),
-            Arguments.of(getClassType(exampleClass), exampleClass, V_OBJECT, ByteBuffer.wrap(serialize(exampleClass))),
+                // className | instance | variantType | variantValue
+                Arguments.of(getClassType(""), null, V_NULL, getClassType("")),
+                Arguments.of(Void.TYPE.getName(), Void.TYPE, V_VOID, VOID_TYPE_VALUE),
+                Arguments.of(getClassType(Boolean.TRUE), Boolean.TRUE, V_BOOLEAN_BOX, Boolean.TRUE),
+                Arguments.of(getClassType(Integer.MAX_VALUE), Integer.MAX_VALUE, V_INT_BOX, Integer.MAX_VALUE),
+                Arguments.of(getClassType(Short.MAX_VALUE), Short.MAX_VALUE, V_SHORT_BOX, Short.MAX_VALUE),
+                Arguments.of(getClassType(Long.MAX_VALUE), Long.MAX_VALUE, V_LONG_BOX, Long.MAX_VALUE),
+                Arguments.of(getClassType(Float.MAX_VALUE), Float.MAX_VALUE, V_FLOAT_BOX, (double) Float.MAX_VALUE),
+                Arguments.of(getClassType(Double.MAX_VALUE), Double.MAX_VALUE, V_DOUBLE_BOX, Double.MAX_VALUE),
+                Arguments.of(getClassType("String"), "String", V_STRING, "String"),
+                Arguments.of(getClassType(exampleClass), exampleClass, V_OBJECT, ByteBuffer.wrap(serialize(exampleClass))),
+                Arguments.of(BigDecimal.class.getTypeName(), V_BIG_DECIMAL, exampleBigDecimal),
 
-            Arguments.of(
-                getClassType(listWithStrings),
-                listWithStrings,
-                V_LIST,
-                new ArrayList() {{
-                    add(new Variant(V_STRING, "1"));
-                    add(new Variant(V_STRING, "2"));
-                }}),
+                Arguments.of(
+                        getClassType(listWithStrings),
+                        listWithStrings,
+                        V_LIST,
+                        List.of(new Variant(V_STRING, "1"),
+                                new Variant(V_STRING, "2")
+                        )),
 
-            Arguments.of(
-                getClassType(mapStringInteger),
-                mapStringInteger,
-                V_MAP,
-                new HashMap() {{
-                    put(new Variant(V_STRING, "one"), new Variant(V_INT_BOX, 1));
-                }}),
+                Arguments.of(
+                        getClassType(mapStringInteger),
+                        mapStringInteger,
+                        V_MAP,
+                        Map.of(new Variant(V_STRING, "one"), new Variant(V_INT_BOX, 1))),
 
-            Arguments.of(
-                getClassType(stringSet),
-                stringSet,
-                V_SET,
-                new HashSet() {{
-                    add(new Variant(V_STRING, "object"));
-                }}),
+                Arguments.of(
+                        getClassType(stringSet),
+                        stringSet,
+                        V_SET,
+                        Set.of(new Variant(V_STRING, "object"))),
 
-            Arguments.of(
-                getClassType(objectsArray),
-                objectsArray,
-                V_ARRAY,
-                new ArrayList() {{
-                    add(new Variant(V_NULL, "general.tests.TestHelper$ExampleClass"));
-                    add(new Variant(V_OBJECT, new object(getClassType(exampleClass), ByteBuffer.wrap(serialize(exampleClass)))));
-                }}
-            ));
+                Arguments.of(
+                        getClassType(objectsArray),
+                        objectsArray,
+                        V_ARRAY,
+                        List.of(
+                                new Variant(V_NULL, "general.tests.TestHelper$ExampleClass"),
+                                new Variant(V_OBJECT, new object(getClassType(exampleClass), ByteBuffer.wrap(serialize(exampleClass))))
+                        )));
     }
 
 
